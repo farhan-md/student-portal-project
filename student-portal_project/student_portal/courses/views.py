b7e4d2ca519883b259f6860ab3bbfc8db51b0c3e
farhan-md <f.majidi1924@gmail.com>
2025-08-27 23:56:37 +0330

    Add views and template

--- student-portal_project/student_portal/courses/views.py
+++ student-portal_project/student_portal/courses/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import course
 # Create your views here.
+def course_list(request):
+    courses = course.objects.all()
+    return render(request,'courses/course_list.html',{'courses': courses})
